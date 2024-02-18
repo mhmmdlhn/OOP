4c2775d61ddad5095ef24aad98ad163186a0bc73
Muhammed <m.muhammedilhan@gmail.com>
2024-02-18 22:07:05 +0300

    Static_Polymorphism

---
+++ 05_Static_Polymorphism/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Static_Polymorphism
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ 05_Static_Polymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿using _05_Static_Polymorphism;
+
+Calculator calculator = new Calculator();
+
+//Example of compile-time polymorphism with method overloading
+int resultInt = calculator.Add(5, 10);
+Console.WriteLine("Integer Total: " + resultInt);
+
+double resultDouble = calculator.Add(2.5, 3.4);
+Console.WriteLine("Double Total: " + resultDouble);

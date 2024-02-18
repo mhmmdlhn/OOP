c94831450ade14d36d84a8dc2f913c5ad0248bef
Muhammed <m.muhammedilhan@gmail.com>
2024-02-18 21:59:51 +0300

    Encapsulation

---
+++ 02_Encapsulation/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Encapsulation
+{
+    public class Car
+    {
+        // Private fields (attributes)
+        private string make;
+        private string model;
+        private int year;
+
+        // Public properties to access the private fields
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                // Example validation: allow setting only if the value is greater than 1900
+                if (value > 1900)
+                {
+                    year = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid year value");
+                }
+            }
+        }
+
+        // Public method
+        public void DisplayCarInfo()
+        {
+            Console.WriteLine($"Make: {Make}, Model: {Model}, Year: {Year}");
+        }
+    }
+}
---
+++ 02_Encapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using _02_Encapsulation;
+
+// Creating an instance of the Car class
+Car myCar = new Car();
+
+// Setting values using properties
+myCar.Make = "Toyota";
+myCar.Model = "Camry";
+myCar.Year = 2022;
+
+// Accessing values using properties
+Console.WriteLine($"Car Info: Make - {myCar.Make}, Model - {myCar.Model}, Year - {myCar.Year}");
+
+// Using a method to display information
+myCar.DisplayCarInfo();
+
+Car car = new();
+car.Make = "Dacia";
+car.Model = "Duster";
+car.Year = 1800;
+
+car.DisplayCarInfo();

c24e9926f3e8b3e85d7ffa243d6d00b47a871844
Muhammed <m.muhammedilhan@gmail.com>
2024-02-18 21:58:12 +0300

    Classes

---
+++ 01_Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using _01_Classes;
+
+// Creating an object of the Student class
+Student student1 = new Student();
+student1.No = 1;
+student1.Name = "A";
+student1.LastName = "AL";
+student1.Age = 18;
+
+student1.PrintStudentInfo();
+
+Student student2 = new();
+student2.No = 2;
+student2.Name = "B";
+student2.LastName = "BL";
+student2.Age = 20;
+
+student2.PrintStudentInfo();
---
+++ 01_Classes/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Classes
+{
+    public class Student
+    {
+        //fields
+        public int No;
+        public string Name;
+        public string LastName;
+
+        //Property prop :
+        public int Age { get; set; }
+
+        //methods
+        public void PrintStudentInfo()
+        {
+            Console.WriteLine($"{No} - Name: {Name}, Last Name: {LastName}, Age: {Age}");
+        }
+    }
+}

9b0e1402e8358cf850522395a98928e8d89e277c
Muhammed <m.muhammedilhan@gmail.com>
2024-02-18 22:14:54 +0300

    Dynamic_Polymorphism

---
+++ 06_Dynamic_Polymorphism/Program.cs
@@ -0,0 +1,7 @@
+﻿using _06_Dynamic_Polymorphism;
+
+Shape shape1 = new Circle();
+Shape shape2 = new Square();
+
+shape1.Draw();  // At runtime, the output will be "Drawing a circle."
+shape2.Draw();  // At runtime, the output will be "Drawing a square."
